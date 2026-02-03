9334a78bc9658316145491f373a31725a1be909c
Student ApnaCollege <133749957+bhupendrasinghpatel@users.noreply.github.com>
2026-02-03 20:11:52 +0530

    Adding Router

--- 07router/src/main.jsx
+++ 07router/src/main.jsx
@@ -7,7 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-
+import Github from './components/Github/Github.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -24,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: 'contact',
         element: <Contact />
+      },
+      {
+        path: 'Github',
+        element: <Github />
       }
     ]
   }
